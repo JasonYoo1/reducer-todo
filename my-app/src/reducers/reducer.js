02a4d479439b5d86ccf2f42c418443ac5a6b445d
JasonYoo1 <jyoojs@gmail.com>
2019-08-13 11:44:07 -0700

    finished step 1

---
+++ my-app/src/reducers/reducer.js
@@ -0,0 +1,15 @@
+// Export Two Things - initialState, reducer fn
+
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+  }
+
+
+  export const reducer = (state, action) => {
+    switch(action.type){
+    default:
+        return state;
+  }
+}
